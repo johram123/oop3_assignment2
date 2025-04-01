7dc2f7dbb07fc4a0e39c6eba949ce8a7daaf19c6
johram123 <johramq@gmail.com>
2025-04-01 09:50:53 -0600

    Initial commit

---
+++ src/utilities/QueueADT.java
@@ -0,0 +1,124 @@
+package utilities;
+
+public interface QueueADT<E>{
+	
+	/**
+	 * Enqueue adds an element to the end of the queue
+	 * 
+	 * @precondition queue must exist
+	 * @postcondition element is successfully added into the queue
+	 * 
+	 * @param element element that will be added
+	 * @throws ValueCannotBeNullException the element value cannot be a null value
+	 */
+	
+	public void enqueue(E element) throws ValueCannotBeNullException;
+	
+	/**
+	 * Dequeue removes the first element from the queue
+	 * 
+	 * @precondition element must exists in a queue
+	 * @postcondition removes the first element from the queue
+	 * 
+	 * @return a new queue with the first element removed
+	 * @throws QueueEmptyException if the queue is empty  
+	 */
+	
+	public E dequeue() throws QueueEmptyException;
+	
+	/**
+	 * Peek allows you to access the first element without removing
+	 * 
+	 * @precondition element must exists in a queue
+	 * @postcondition retrieves the first element
+	 * 
+	 * @return first element gets returned
+	 * @throws QueueEmptyException cannot access an element if the queue is empty
+	 */
+	
+	public E peek() throws QueueEmptyException;
+	
+	/**
+	 * Equals compares 2 queue's and checks if the queue are the same length size and order
+	 * 
+	 * @precondition 2 queue must exists
+	 * @postcondition gives a boolean value to indicate the result of comparison
+	 * 
+	 * @param that that the queue is to be compared with
+	 * @return returns true if both queue are exactly same, returns false if queue are different
+	 * @throws InvalidQueueException if the provided queue is invalid/ if one exist and the other does not exist 
+	 */
+	
+	public boolean equals(MyQueueADT<E> that) throws InvalidQueueException;
+	
+	/**
+	 * Iterator iterates over the elements in the queue
+	 * 
+	 * @precondition queue must exists
+	 * @postcondition gain access to the next element
+	 * 
+	 * @return an iterator over the elements contained in the queue
+	 */
+	
+	public Iterator<E> iterator();
+	
+	/**
+	 * toArray turns all elements in the queue to an array
+	 * 
+	 * @precondition queue must exists
+	 * @postcondition gives an array containing all elements in the queue
+	 * 
+	 * @return Array that completely replicates and converts the queue into an array. Same order and size
+	 */
+	
+	public Object[] toArray();
+	
+	/**
+	 * Copy the elements of the queue into provided array
+	 * 
+	 * @precondition queue must exists
+	 * @postcondition gives an array with the same elements of the queue
+	 * 
+	 * @param copy copy the queue element into the array
+	 * @return an array containing all elements of the queue
+	 */
+	
+	public E[] toArray(E[] copy);
+
+	/**
+	 * isFull checks if the queue is full
+	 * 
+	 * @precondition queue must exist
+	 * @postcondition gives an boolean value depending on the result
+	 * 
+	 * @return True if the queue is full, False if not full
+	 */
+	
+	public boolean isFull();
+	
+	/**
+	 * Sizes checks the size of the queue
+	 * 
+	 * @precondition queue must exist
+	 * @postcondition int value must be given 
+	 * 
+	 * @return an int value depending on the size of the queue is returned
+	 */
+	
+	
+	public int size();
+	
+	/**
+	 * dequeueAll removes all the elements in the queue
+	 * 
+	 * @precondition the queue must exists and is not empty
+	 * @postcondition queue must be empty
+	 * 
+	 * @return a empty queue
+	 * @throws QueueEmptyException if the queue is already empty
+	 */
+
+	public E dequeueAll() throws QueueEmptyException;
+	
+	
+}
---
+++ src/utilities/StackADT.java
@@ -0,0 +1,157 @@
+package utilities;
+
+public interface StackADT<E> {
+	/**
+	 * Constructor method to make a Stack object
+	 * 
+	 * Precondition: none
+	 * Postcondition: A Stack object is created and its initial value is set to the provided
+	 * size in the parameter.
+	 * 
+	 * @param size The size of the new stack
+	 */
+	public void create(int size);
+	
+	/**
+	 * Mutator method that adds a new element to the top of the stack.
+	 * 
+	 * Precondition: Stack object exists and a non-null value was provided to the parameters.
+	 * Postcondition: Element is successfully added to the top of the stack.
+	 * 
+	 * @param element Element to be added
+	 * @return boolean If element was successfully added
+	 * 
+	 * @throws ElementCannotBeNullException is thrown if provided element is null
+	 */
+
+	public boolean push(E element) throws ElementCannotBeNullException;
+	
+	/**
+	 * Mutator method that removes the topmost element.
+	 * 
+	 * Precondition: Stack object exists.
+	 * Postcondition: The topmost element is removed from the stack
+	 * 
+	 * @return E the item that was removed
+	 * 
+	 * @throws StackEmptyException is thrown if Stack is empty
+	 */
+
+	public E pop() throws StackEmptyException;
+	
+	/**
+	 * Accessor method that retrieves the value of the topmost element.
+	 * 
+	 * Precondition: Stack object exists
+	 * Postcondition: The topmost element is accessed and return from the stack.
+	 * 
+	 * @return E the element that was retrieved
+	 * 
+	 * @throws StackEmptyException is thrown if Stack is empty.
+	 */
+
+	public E peek() throws StackEmptyException;
+	
+	/**
+	 * Transformer method that compares two Stack objects
+	 * 
+	 * Precondition: At least two stack objects exists
+	 * Postcondition: A boolean is returned if two Stack objects are the same, i.e, same length and in same order.
+	 * 
+	 * @param that the Stack object the current Stack is being compared to
+	 * @return boolean if the two Stack objects are the same.
+	 * 
+	 * @throws InvalidStackCompareException is thrown if the Stack being compared to is invalid, i.e, does not exist.
+	 */
+
+	public boolean equals(StackADT<E> that) throws InvalidStackCompareException;
+	
+	/**
+	 * Transformer method that returns an iterator over the items contained in the Stack.
+	 * 
+	 * Precondition: Stack object exists.
+	 * Postcondition: Iterator object is returned that allows for iteration over the items
+	 * 
+	 * @return Iterator object that allows iteration.
+	 */
+
+	public Iterator<E> iterator();
+	
+	/**
+	 * Mutator method that converts the Stack to an array
+	 * 
+	 * Precondition: Stack object exists
+	 * Postcondition: Stack is converted to an array.
+	 * 
+	 * @return Object array to hold the Stack elements with generic data type.
+	 */
+	public Object[] toArray();
+	
+	/**
+	 * Mutator method that copies elements from a Stack to an existing array.
+	 * 
+	 * Precondition: Stack object exists and a valid array is provided.
+	 * Postcondition: Stack is successfully copied
+	 * 
+	 * @param copy designated array that takes in the copied elements from Stack
+	 * @return E[] array to hold the Stack elements with specific data type.
+	 */
+
+	public E[] toArray(E[] copy);
+	
+	/**
+	 * Accessor method that searches for the element provided.
+	 * 
+	 * Precondition: Stack object exists and a valid element is provided.
+	 * Postcondition: Element is successfully found are returned.
+	 * 
+	 * @param obj object to be searched
+	 * @return the position of the object (starting from 1)
+	 */
+
+	public int search(E obj);
+	
+	/**
+	 * Accessor method that returns if provided element exists in the Stack
+	 * 
+	 * Precondition: Stack object exists and valid object is provided.
+	 * Postcondition: A boolean is returned if object exists or not in the element
+	 * 
+	 * @param obj object to be searched
+	 * @return boolean value if object exists or not
+	 */
+
+	public boolean contains(E obj);
+	
+	/**
+	 * Accessor method that retrieves the size of the Stack.
+	 * 
+	 * Precondition: Stack object exists.
+	 * Postcondition: The size of the Stack is retrieved.
+	 * 
+	 * @return the size of the Stack
+	 */
+
+	public int size();
+	
+	/**
+	 * Transformer method that returns if Stack is empty or not.
+	 * 
+	 * Precondition: Stack object exists.
+	 * Postcondition: A boolean is returned if Stack is empty or not
+	 * 
+	 * @return boolean value if object exists or not
+	 */
+
+	public boolean isEmpty();
+	
+	/**
+	 * Mutator method that empties the stack
+	 * 
+	 * Precondition: Stack object exists.
+	 * Postcondition: Stack is emptied.
+	 */
+
+	public void clear();
+
+}
